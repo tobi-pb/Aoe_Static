9eb9669ce3608e4e36b285f8a0e2a454977e9623
thebod <bastian.ike@googlemail.com>
2014-01-10 12:36:03 +0100

    finished aoe_static_model_config testing

--- app/code/community/Aoe/Static/Test/Model/Config.php
+++ app/code/community/Aoe/Static/Test/Model/Config.php
@@ -30,4 +30,40 @@ class Aoe_Static_Test_Model_Config extends EcomDev_PHPUnit_Test_Case
         $config->setNode('aoe_static_purging', null);
         $this->assertEmpty($config->getAdapters());
     }
+
+    public function test_getActionConfiguration()
+    {
+        $config = Mage::getModel('aoestatic/config');
+
+        $config->setNode('aoe_static/testaction1/disabled', true);
+        $config->setNode('aoe_static/testaction2', 'test-config');
+        $config->setNode('aoe_static/testaction3/use', 'testaction2');
+
+        $this->assertFalse($config->getActionConfiguration('testaction0'));
+        $this->assertFalse($config->getActionConfiguration('testaction1'));
+        $this->assertEquals('test-config', $config->getActionConfiguration('testaction2'));
+        $this->assertEquals('test-config', $config->getActionConfiguration('testaction3'));
+    }
+
+    public function test_getMarkersCallbackConfiguration()
+    {
+        $config = Mage::getModel('aoestatic/config');
+
+        $config->setNode('aoe_static/default/markers', 'aaa');
+
+        $this->assertEquals('aaa', $config->getMarkersCallbackConfiguration());
+    }
+
+    public function test_getMarkerCallback()
+    {
+        $config = Mage::getModel('aoestatic/config');
+
+        $config->setNode('aoe_static/default/markers/m1/valueCallback', 'test-callback');
+
+        $this->assertEquals('', $config->getMarkerCallback('aaa'));
+        $this->assertEquals('', $config->getMarkerCallback('###aaa'));
+
+        $this->assertEquals('test-callback', $config->getMarkerCallback('m1'));
+        $this->assertEquals('test-callback', $config->getMarkerCallback('#m#1###'));
+    }
 }
